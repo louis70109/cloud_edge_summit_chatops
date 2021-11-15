6e77bdc5a16d263280ea0ac7b4892c32fc0acf71
louis70109 <nijia.lin@linecorp.com>
2021-11-15 18:56:07 +0800

    fix typo

--- bot_proxy/api.py
+++ bot_proxy/api.py
@@ -45,7 +45,7 @@ def callback():
     signature = request.headers['X-Line-Signature']
 
     # get request body as text
-    body = request.get_datsa(as_text=True)
+    body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
     # handle webhook body
